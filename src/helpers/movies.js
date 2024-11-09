5e09b2d27f4044ebde250bc53d130393b42f7b8b
Anton Mamontov <a.mamontov@p-s.kz>
2024-11-09 16:06:08 +0500

    components will adding

---
+++ src/helpers/movies.js
@@ -0,0 +1,18 @@
+
+import { locations } from "../data/data.js"
+
+export function goTown() {
+  return locations[0].name
+}  
+
+export function goStore() {
+    return locations[1].name
+  }
+  
+export  function goCave() {
+    return locations[2].name
+  }
+  
+export function fightDragon() {
+    return locations[3].name
+  }
